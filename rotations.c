87d2fcbd3f22dc50a0e3f41c5abb1c71f07789c3
Youssef Agnaou <Agnaou004@gmail.com>
2022-04-15 01:45:48 +0000

    rotations

--- rotations.c
+++ rotations.c
@@ -6,7 +6,7 @@
 /*   By: yagnaou <yagnaou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 01:21:06 by yagnaou           #+#    #+#             */
-/*   Updated: 2022/04/13 01:55:28 by yagnaou          ###   ########.fr       */
+/*   Updated: 2022/04/15 01:43:33 by yagnaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,43 +47,5 @@ void    rotate_both_a_and_b(t_list **lsta, t_list **lstb, int index_a, int index
     {
         rotate_b(lstb);
         index_b--;
-    }
-}
-
-void    reverse_rotate_a_to_top(t_list **lst, int size, int index)
-{
-    while (index < size)
-    {
-        reverse_rotate_a(lst);
-        index++;
-    }
-}
-
-void    reverse_rotate_b_to_top(t_list **lst, int size, int index)
-{
-    while (index < size)
-    {
-        reverse_rotate_b(lst);
-        index++;
-    }
-}
-
-void    reverse_rotate_both_a_and_b(t_list **lsta, t_list **lstb, int index_a, int index_b, int size_a, int size_b)
-{
-    while (index_a < size_a && index_b < size_b)
-    {
-        reverse_rotate_a_and_b(lsta, lstb);
-        index_a++;
-        index_b++;
-    }
-    while (index_a < size_a)
-    {
-        reverse_rotate_a(lsta);
-        index_a++;
-    }
-    while (index_b < size_b)
-    {
-        reverse_rotate_b(lstb);
-        index_b++;
     }
 }
